9c1db7c1d9d5a8377a170cf3286dfbbe787ec686
Hasan Gunes <hasangunes987@gmail.com>
2025-03-31 22:07:48 +0300

    Added cards class the load all game cards on that class

--- card.py
+++ card.py
@@ -1,8 +1,23 @@
+import os
 import pygame
 
 class Card(pygame.sprite.Sprite):
     def __init__(self, image_path):
-        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_path).convert()
         self.rect = self.image.get_rect()
 
+class Cards:
+    def __init__(self):
+        self.cards = pygame.sprite.Group()
+        self.load_cards()
+
+    def load_cards(self):
+        card_images = os.listdir(path="assets/cards")
+
+        for card in card_images:
+            card = Card(f"assets/cards/{card}")
+            self.cards.add(card)
+
+    def select_card(self, card): 
+        pass
