52cd9d88ecf797c67ae586d364b3fae288647b6b
Hasan Gunes <hasangunes987@gmail.com>
2025-03-31 22:08:22 +0300

    Remove the card loading on the init

--- deck.py
+++ deck.py
@@ -1,21 +1,14 @@
 import pygame
 import random
-from card import Card
+from card import Cards
 
 class Deck:
     
     def __init__(self):
-        self.cards = pygame.sprite.Group()
-        card_images = [
-            "assets/Cards/card_1.png",
-            "assets/Cards/card_2.png",
-            "assets/Cards/card_3.png",
-            "assets/Cards/card_4.png",
-            "assets/Cards/card_5.png",
-        ]
-        for img in card_images:
-            card = Card(img)
-            self.cards.add(card)
+        self.cards = Cards().cards
+
+    def build_deck(self):
+        self.cards.add()
 
     def shuffle_deck(self):
         shuffled_cards = list(self.cards)
