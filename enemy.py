bb996f5e0aa56228f444c80ae7ec2b9d3aa84985
Hasan Gunes <hasangunes987@gmail.com>
2025-04-01 21:20:25 +0300

    Enemy

---
+++ enemy.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__
+        self.image = pygame.image.load("assets/enemies/angry_man.png").convert()
+        self.rect = self.image.get_rect()
+
+    def adjust_enemy_position(self, screen_height):
+        self.rect.top = (screen_height - self.rect.height) // 2
+        self.rect.left = 1050
