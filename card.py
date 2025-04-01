4a1a884f9ef03a2e95167cf908c4c4e5c455e06f
Hasan Gunes <hasangunes987@gmail.com>
2025-04-01 21:21:32 +0300

    Added copy image and selected attribute

--- card.py
+++ card.py
@@ -4,8 +4,10 @@ import pygame
 class Card(pygame.sprite.Sprite):
     def __init__(self, image_path):
         pygame.sprite.Sprite.__init__(self)
+        self.original_image = pygame.image.load(image_path).convert()
         self.image = pygame.image.load(image_path).convert()
         self.rect = self.image.get_rect()
+        self.selected = False
 
 class Cards:
     def __init__(self):
@@ -15,9 +17,9 @@ class Cards:
     def load_cards(self):
         card_images = os.listdir(path="assets/cards")
 
-        for card in card_images:
-            card = Card(f"assets/cards/{card}")
-            self.cards.add(card)
+        for image in card_images:
+            image = Card(f"assets/cards/{image}")
+            self.cards.add(image)
 
     def select_card(self, card): 
         pass
