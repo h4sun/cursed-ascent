278327d7f2e94ff1553a35ed7aec1ec0d5615e30
Hasan Gunes <hasangunes987@gmail.com>
2025-04-01 21:22:19 +0300

    Added draw method and also added selected card method to select a car

--- hand.py
+++ hand.py
@@ -13,32 +13,50 @@ class Hand:
         else:
             self.cards.add(deck.cards)   
           
-    def adjust_card_position(self, screen_width):
-        card_top = 600
+
+    def draw(self, surface, screen_width):
         num_cards = len(self.cards)
         
         # Calculate the initial left position relative to the center of the screen
         total_width = (num_cards * 150) + (num_cards - 1)
         start_x = (screen_width - total_width) // 2
 
+        card_top = 600
         current_x = start_x
-        for i, card in enumerate(self.cards):
+        for card in self.cards:
             card.rect.left = current_x
-            if i % 2 == 0:
-                card.rect.top = card_top - 5
-            else:
-                card.rect.top = card_top
-
-            # Adding the gap between the cards
             current_x += card.rect.width
+            if not card.selected:
+                card.rect.top = card_top
+            else:
+                if card.rect.top > 500:
+                    print(card.rect.top)
+                    card.rect.top -= 100
+                
+
+        # Enlarge the card when the mouse on the card
+        mouse_pos = pygame.mouse.get_pos()
+        for card in self.cards:
+            if not card.selected:
+                if card.rect.collidepoint(mouse_pos):
+                    card.rect.top -= 100
 
 
+        # Draw
+        self.cards.draw(surface)
 
+    def select_card(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
 
-
-
-
-
+            for card in self.cards:
+                if card.rect.collidepoint(mouse_pos):
+                    card.selected = not card.selected
+                    if card.selected:
+                        card.image.fill("red")
+                    else:
+                        card.image = card.original_image.copy()
+                    print(f"Card selected: {card.selected}")
 
 
 
