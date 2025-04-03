b937610a22e8381a6836245c3f7eff283f211df0
Hasan Güneş <hasangunes@192.168.2.27>
2025-04-03 23:42:13 +0300

    Added type attribute and try to insert type to cards

--- card.py
+++ card.py
@@ -2,10 +2,11 @@ import os
 import pygame
 
 class Card(pygame.sprite.Sprite):
-    def __init__(self, image_path):
+    def __init__(self, image_path, type):
         pygame.sprite.Sprite.__init__(self)
         self.original_image = pygame.image.load(image_path).convert()
         self.image = pygame.image.load(image_path).convert()
+        self.type = type
         self.rect = self.image.get_rect()
         self.selected = False
 
@@ -17,9 +18,9 @@ class Cards:
     def load_cards(self):
         card_images = os.listdir(path="assets/cards")
 
-        for image in card_images:
-            image = Card(f"assets/cards/{image}")
+        for index, image in enumerate(card_images):
+            if index % 2 == 0:
+                image = Card(f"assets/cards/{image}", "attack")
+            else:
+                image = Card(f"assets/cards/{image}", "defense")
             self.cards.add(image)
-
-    def select_card(self, card): 
-        pass
