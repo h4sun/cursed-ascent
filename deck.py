dae8b1b9076af4cbf13ff2fb63982d4987909452
Hasan Gunes <hasangunes987@gmail.com>
2025-04-03 13:09:17 +0300

    Checked the function name draw_card to draw_card_from_deck

--- deck.py
+++ deck.py
@@ -15,7 +15,7 @@ class Deck:
         random.shuffle(shuffled_cards)
         self.cards = pygame.sprite.Group(shuffled_cards)
 
-    def draw_card(self):
+    def draw_card_from_deck(self):
         if self.cards:
             card = random.choice(list(self.cards))
             return card
