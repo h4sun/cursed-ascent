62b6a4b29dec34a037703fd44ad4a45c974fb499
Hasan Gunes <hasangunes987@gmail.com>
2025-04-04 23:29:02 +0300

    Added armor and live attributes

--- enemy.py
+++ enemy.py
@@ -6,10 +6,12 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.image.load("assets/enemies/angry_man.png").convert()
         self.rect = self.image.get_rect()
         self.turn = False
+        self.live = True
         self.adjust_enemy_position(720)
 
         # Enemy Stats
         self.hp = 10
+        self.armor = 0
         self.damage = 5
 
     def adjust_enemy_position(self, screen_height):
