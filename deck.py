733c9e77032619420ac6aa9c0135853a6700e8f0
Hasan Gunes <hasangunes987@gmail.com>
2025-04-04 23:28:26 +0300

    Added card grave still have some issues

--- deck.py
+++ deck.py
@@ -6,6 +6,7 @@ class Deck:
     
     def __init__(self, cards):
         self.deck_cards = cards
+        self.card_graveyard = []
 
     def build_deck(self):
         self.deck_cards.add()
@@ -16,13 +17,17 @@ class Deck:
         self.deck_cards = pygame.sprite.Group(shuffled_cards)
 
     def draw_card(self):
-        if self.deck_cards:
-            card = random.choice(list(self.deck_cards))
+        if len(self.deck_cards) > 0:
+            card = self.deck_cards.sprites()[0]
             return card
         else:
-            return None
+            self.deck_cards = pygame.sprite.Group(self.card_graveyard) # Hangi kart en üste geliyor bak ilk giren mi yoksa son giren mi
+            self.card_graveyard = []
+            card = self.deck_cards.sprites()[0]
+            return card
 
     def remove_card(self, card):
+        self.card_graveyard.append(card)
         self.deck_cards.remove(card)
 
 
