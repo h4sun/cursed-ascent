3a368734788e22fc35fc51688f7dc33e265ff3ef
Hasan Gunes <hasangunes987@gmail.com>
2025-04-04 13:08:18 +0300

    Made the hand.py more flex and checking mechanics on the game class

--- hand.py
+++ hand.py
@@ -1,16 +1,13 @@
 import pygame
-from character import Character
-from enemy import Enemy
 
 class Hand:
 
-    def __init__(self):
+    def __init__(self,):
         self.hand_cards = pygame.sprite.Group()
         self.hand_copy = []
 
-        self.player = Character()
-        self.enemy = Enemy()
-
+        self.selected_card = None
+        self.released_card = False 
 
     def draw_hand(self, deck, hand_size):
         if (len(deck.deck_cards) >= hand_size):
@@ -25,7 +22,6 @@ class Hand:
         for card in self.hand_cards:
             self.hand_copy.append(card) # Copy hand it is solve the topmost card when the select
           
-
     def draw(self, surface, screen_width):
         num_cards = len(self.hand_cards)
         
@@ -34,8 +30,7 @@ class Hand:
         start_x = (screen_width - total_width) // 2
 
         # Adjust the position of the cards
-        get_selected_card = self.__get_selected_card()
-        if get_selected_card == None:
+        if self.selected_card == None:
             card_top = 600
             current_x = start_x
             for card in self.hand_cards:
@@ -43,63 +38,47 @@ class Hand:
                 current_x += card.rect.width
                 card.rect.top = card_top
                 
-
         # Kart secili değilken üstüne geldiğinde kartı y ekseninde biraz büyüt eğer kart secili ise mousela birlikte kartı hareket ettir.
         # If the card is not selected give some enlarging when you hovering the mouse on the cards if the card is selected move the card with the mouse.
         mouse_pos = mouse_x, mouse_y = pygame.mouse.get_pos()
-        if get_selected_card == None:
+        if self.selected_card == None:
             for card in self.hand_cards:
                 if card.rect.collidepoint(mouse_pos): 
                     card.rect.top -= 100
         else:
-            get_selected_card.rect.left = mouse_x - (get_selected_card.rect.width / 2)
-            get_selected_card.rect.top = mouse_y - (get_selected_card.rect.height / 2)
+            self.selected_card.rect.left = mouse_x - (self.selected_card.rect.width / 2)
+            self.selected_card.rect.top = mouse_y - (self.selected_card.rect.height / 2)
 
         self.hand_cards.draw(surface)
 
-    def select_card(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            mouse_pos = pygame.mouse.get_pos()
-            
-            get_selected_card = self.__get_selected_card()
-            if get_selected_card == None:
-                for card in self.hand_cards:
-                    if card.rect.collidepoint(mouse_pos):
-                        self.hand_cards.remove(card)
-                        self.hand_cards.add(card)
-                        card.selected = True
-                        card.image.fill("red")
-            else:
-                self.hand_cards.empty()
-                self.hand_cards = pygame.sprite.Group(self.hand_copy)
-                self.check_card_collide(get_selected_card, self.player, self.enemy)
-                get_selected_card.selected = False
-                get_selected_card.image = get_selected_card.original_image.copy()
+    def select_card(self):
+        mouse_pos = pygame.mouse.get_pos()
 
-
-    def check_card_collide(self, card, player, enemy):
-        if card.type == "attack":
-            if card.rect.colliderect(enemy.rect):
-                self.hand_cards.remove(card)
-                self.hand_copy.remove(card)
-        elif card.type == "defense":
-            if card.rect.colliderect(player.rect):
-                self.hand_cards.remove(card)
-                self.hand_copy.remove(card)
-
-
-
-    def __get_selected_card(self):
-        for card in self.hand_cards:
-            if card.selected:
-                return card
+        if self.selected_card == None:
+            for card in self.hand_cards:
+                if card.rect.collidepoint(mouse_pos):
+                    self.selected_card = card
+                    #card.image.fill("red")
+                    self.hand_cards.remove(card)
+                    self.hand_cards.add(card)
+                    return False # We still holding the card
+           
         else:
+            #self.selected_card.image = self.selected_card.original_image.copy()
+            #print(self.selected_card.rect)
+            return True # We releasing the card
+
+
+
+    # Realease the card and return the realesed card
+    def release_card(self):
+        if self.selected_card == None:
             return None
-    
 
+        released_card = self.selected_card   
+        self.selected_card = None
+        self.hand_cards = pygame.sprite.Group(self.hand_copy)
 
-
-
-
+        return released_card
 
 
