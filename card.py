894f07669322b674d93b31fb491c0ccb376a4a4d
Hasan Gunes <hasangunes987@gmail.com>
2025-04-04 13:07:05 +0300

    Deleted the select attribute

--- card.py
+++ card.py
@@ -2,13 +2,15 @@ import os
 import pygame
 
 class Card(pygame.sprite.Sprite):
+
     def __init__(self, image_path, type):
         pygame.sprite.Sprite.__init__(self)
         self.original_image = pygame.image.load(image_path).convert()
         self.image = pygame.image.load(image_path).convert()
         self.type = type
         self.rect = self.image.get_rect()
-        self.selected = False
+
+
 
 class Cards:
     def __init__(self):
