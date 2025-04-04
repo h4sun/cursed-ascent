a9173c3c03d5bf9edbd1c2aaa1cf461823a9ad99
Hasan Gunes <hasangunes987@gmail.com>
2025-04-04 16:54:19 +0300

    Added simple functionality to enemy class

--- enemy.py
+++ enemy.py
@@ -5,9 +5,19 @@ class Enemy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__
         self.image = pygame.image.load("assets/enemies/angry_man.png").convert()
         self.rect = self.image.get_rect()
+        self.turn = False
         self.adjust_enemy_position(720)
 
+        # Enemy Stats
+        self.hp = 10
+        self.damage = 5
 
     def adjust_enemy_position(self, screen_height):
         self.rect.top = (screen_height - self.rect.height) // 2
         self.rect.left = 1050
+
+
+    def attack(self, target):
+        target.hp -= 5
+
+
