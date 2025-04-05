4cb7829634f0b31f510bf33ddedafc37682570b5
Hasan Gunes <hasangunes987@gmail.com>
2025-04-05 22:24:11 +0300

    Added function to draw text and menu functions

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from player import Player
 from button import Button
 from card import Cards
 
+pygame.init()
+pygame.display.set_caption("Cursed Ascent")
+
 class Game:
 
     def __init__(self):
-        pygame.init()
+
         self.SCREEN_SIZE = self.SCREEN_WIDTH, self.SCREEN_HEIGHT = (1280, 720)
         self.screen = pygame.display.set_mode(self.SCREEN_SIZE)
         self.clock = pygame.time.Clock()
@@ -28,12 +31,15 @@ class Game:
         self.deck.shuffle_deck()
 
         # Initialize buttons
-        self.button = Button(1100, 600)
+        self.button = Button("assets/buttons/end_turn.png", 1100, 600)
 
         # Initialize hand
         self.hand = Hand()
         self.hand.draw_hand(self.deck)
 
+        self.font = pygame.font.SysFont(None, 20)
+
+    # Drawing
     def draw(self):
         if self.enemy.live:
             self.screen.blit(self.enemy.image, self.enemy.rect)
@@ -47,8 +53,15 @@ class Game:
             
         self.screen.blit(self.button.image, self.button.rect)
 
+    def draw_text(self, text, font, color, surface, x, y):
+        textobj = font.render(text, 1, color)
+        textrect = textobj.get_rect()
+        textrect.topleft = (x, y)
+        surface.blit(textobj, textrect)
+
+    # Events
     def handle_button_events(self, event):
-        clicked = self.button.click(event)
+        clicked = self.button.is_clicked(event)
 
         if clicked and self.player.turn == True:
             self.hand.draw_hand(self.deck)
@@ -66,6 +79,7 @@ class Game:
                         self.apply_card_effects(released_card, self.enemy)
 
 
+    # Game logic
     def apply_card_effects(self, card, target):
         if card.type == "attack":
             target.hp -= 5
@@ -74,6 +88,7 @@ class Game:
             target.armor += 5
 
         self.hand.hand_cards.remove(card)
+        self.deck.card_graveyard.append(card)
         self.hand.hand_copy.remove(card)
 
     def enemy_logic(self):
@@ -86,7 +101,41 @@ class Game:
             self.player.turn = True
 
 
-    def run(self):
+    # Menu
+
+    def quit(self):
+        pygame.quit()
+
+    def main_menu(self):
+        running = True
+        while running:
+
+            self.screen.fill((0, 0, 0))
+
+            self.play_button = Button("assets/buttons/play.png", self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2)
+            self.quit_button = Button("assets/buttons/quit.png", self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2 + 200)
+
+    
+
+            self.screen.blit(self.play_button.image, self.play_button.rect)
+            self.screen.blit(self.quit_button.image, self.quit_button.rect)
+
+            # Pool Events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                
+                if self.play_button.is_clicked(event):
+                    game.game()
+
+                if self.quit_button.is_clicked(event):
+                    running = False
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        
+    def game(self):
         running = True
         while running:
             self.screen.fill((0, 0, 0))
@@ -107,10 +156,7 @@ class Game:
             self.clock.tick(60)
 
 
-        pygame.quit()
-
-
-
 game = Game()
 if __name__ == "__main__":
-    game.run()
+    game.main_menu()
+    pygame.quit()
