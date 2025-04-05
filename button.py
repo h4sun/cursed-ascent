7a19d25f851b5b50a8cc50481b3cbcbfc6567118
Hasan Gunes <hasangunes987@gmail.com>
2025-04-05 22:22:30 +0300

    Change button init class

--- button.py
+++ button.py
@@ -2,17 +2,16 @@ import pygame
 
 class Button():
 
-    def __init__(self, x, y):
-        self.image = pygame.image.load("assets/buttons/end_turn.png").convert()
+    def __init__(self, image_path, x, y):
+        self.image = pygame.image.load(image_path).convert()
+        #self.image = pygame.transform.scale(self.image, (1280, 720))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
 
-    def click(self, event):
-
+    def is_clicked(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_pos = pygame.mouse.get_pos()
 
             if self.rect.collidepoint(mouse_pos):
                 return True
-        
